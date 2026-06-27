583810bec37ded7f311c6942700a39c468a32549
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2026-06-27 14:57:19 +0300

    practicing object oriented programin with python

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,14 +1,25 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+segments = []
+game_is_on = True
+new_snake = Snake()
 
 screen = Screen()
-
-positions = [(0, 0), (-20, 0), (-40, 0)]
-for curr_position in positions:
-    segment = Turtle("square")
-    segment.color("white")
-    segment.goto(curr_position)
-
-screen.bgcolor("black")
 screen.setup(width=600, height=600)
-screen.title("Snake game!")
-screen.exitonclick()
+screen.bgcolor("black")
+screen.title("Snake Game!")
+screen.tracer(0)
+
+screen.listen()
+
+screen.onkey(new_snake.up, "Up")
+screen.onkey(new_snake.down, "Down")
+screen.onkey(new_snake.left, "Left")
+screen.onkey(new_snake.right, "Right")
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    new_snake.move()
