075b7e2e7ded069e5430e1ef7cbe3c609e496c78
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2026-07-05 20:46:42 +0300

    practicing object oriented programin with python

--- files-edit/main.py
+++ files-edit/main.py
@@ -1,5 +1,9 @@
-with open("file_to_read.txt", mode="r") as file:
-    file.seek(0)  # Move the cursor back to the beginning
+with open("C:/Users/marko/OneDrive/Desktop/file_to_read.txt", "r+") as file:
+    # file.write("\nAdd some new line to this file.")
+    # file.write("\nAdd another line before seeking and then reading.")
+    file.seek(
+        0)  # Returns the cursor to the beginning of the file so that everything from top to bottom can print out from line 9
+
     content = file.read()
 
 print(content)
